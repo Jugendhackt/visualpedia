80fb21ba7c8565a8c89bbf321dcb08eee2db9b3c
BuildTools <unconfigured@null.spigotmc.org>
2019-05-25 11:42:51 +0200

    Update pages.js

---
+++ backend/pages.js
@@ -0,0 +1,12 @@
+
+function getBasicInformation(page){
+    $.ajax({
+        url: '//en.wikipedia.org/w/api.php',
+        data: { action: 'query', list: 'search', srsearch: $("input[name=Wikipedia]").val(), format: 'json' },
+        dataType: 'jsonp',
+        success: function (x) {
+            console.log('title', x.query.search[0].title);
+        }
+    });
+    return null;
+}
