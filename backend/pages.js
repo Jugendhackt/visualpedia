387500a70ec0f6e2b98b08038f6ca27c6dab07bc
BuildTools <unconfigured@null.spigotmc.org>
2019-05-25 15:24:41 +0200

    backend changes

--- backend/pages.js
+++ backend/pages.js
@@ -1,6 +1,5 @@
 
 function getSummary(page,callback){
-    var summary;
     var wikiURL = "https://de.wikipedia.org/w/api.php";
     wikiURL += '?' + $.param({
         'action' : 'query',
@@ -20,6 +19,24 @@ function getSummary(page,callback){
                 var substr = data.query.pages[a].extract.split('\n');
                 callback(substr[0]);
             }
+        }
+    });
+}
+function getTopics(page,callback){
+    var summary;
+    var wikiURL = "https://de.wikipedia.org/w/api.php";
+    wikiURL += '?' + $.param({
+        'action' : 'parse',
+        'page' : page,
+        'format':'json'
+    });
+    $.ajax( {
+        async: false,
+        url: wikiURL,
+        dataType: 'jsonp',
+        async:false,
+        success: function(data){
+            console.log(data);
         }
     });
 }
